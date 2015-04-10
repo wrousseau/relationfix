f307463c4ea7241f8a1d4604945193a0eb634441
Woody Rousseau <woody.rousseau@ensta-paristech.fr>
2015-04-10 16:31:04 +0200

    Setup code coverage using Istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -27,14 +27,22 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'client/js/**/*.js': ['coverage'],
+      'common/models/*.js': ['coverage'],
+      'server/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['coverage'],
 
+    // coverage reporter and output directory
+    coverageReporter : {
+      type: 'html',
+      dir: 'coverage/'
+    },
 
     // web server port
     port: 9876,
